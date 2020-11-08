2e3f7b42219e9636947d1296c9231d8fd8e7f34b
nyoongoon <nyoongoon@naver.com>
2020-11-08 16:38:42 +0900

    exercise_04_2_3_after

--- exercise_04_2_3.py
+++ exercise_04_2_3.py
@@ -2,11 +2,18 @@
 numbers = [1,2,6,8,4,3,2,1,9,5,4,9,7,2,1,3,5,4,8,9,7,2,3]
 counter = {}
 
-for number in numbers:
-    if counter.get(number) == None:
-        counter[number] = 1
-    else:
-        counter[number] += 1
+# 나의 답: 딕셔너리.get(키)로 접근.
+# for number in numbers:
+#     if counter.get(number) == None:
+#         counter[number] = 1
+#     else:
+#         counter[number] += 1
 
+# 해답: if ~ in으로 접근.
+for number in numbers:
+    if number in counter:
+        counter[number] = counter[number] + 1
+    else:
+        counter[number] = 1
 
 print(counter)
