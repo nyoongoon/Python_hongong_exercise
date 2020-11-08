da9363a646fc8defe12a186693f28db9aa624df8
nyoongoon <nyoongoon@naver.com>
2020-11-08 14:52:15 +0900

    exercise_04_1_after

--- exercise_04_1_3.py
+++ exercise_04_1_3.py
@@ -5,9 +5,13 @@ for number in numbers:
     else:
         print("{}는 홀수입니다".format(number))
 
-    if 1000 > number >= 100:
-        print("{}는 3자릿수입니다".format(number))
-    elif number >= 10:
-        print("{}는 2자릿수입니다".format(number))
-    elif number > 0:
-        print("{}는 1자릿수입니다".format(number))
+    # if 1000 > number >= 100:
+    #     print("{}는 3자릿수입니다".format(number))
+    # elif number >= 10:
+    #     print("{}는 2자릿수입니다".format(number))
+    # elif number > 0:
+    #     print("{}는 1자릿수입니다".format(number))
+
+for number in numbers:
+    print(number, "는", len(str(number)), "자릿수입니다")
+    # str()로 문자열 변환 후, len()로 길이 계산
--- exercise_04_1_5.py
+++ exercise_04_1_5.py
@@ -1,7 +1,10 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 output = [[], [], []]
 
+# for number in numbers:
+#     output[(number%3)-1].append(number)
+
 for number in numbers:
-    output[(number%3)-1].append(number)
+    output[(number+2)%3].append(number)
 
 print(output)
