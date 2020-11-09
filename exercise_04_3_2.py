f0b4cb51b82b3c66b6ddea7e38d6d34f72bbf4db
nyoongoon <nyoongoon@naver.com>
2020-11-09 14:16:46 +0900

    exercise_04_3_2_after

--- exercise_04_3_2.py
+++ exercise_04_3_2.py
@@ -3,8 +3,6 @@ value_list = ["기사", 200, 30, 5]
 character = {}
 
 for i in range(len(key_list)):
-        character = {
-            key_list[i] : value_list[i]
-        }
-
+    character[key_list[i]] = value_list[i]
+# 딕셔너리에 요소 추가 방법 틀렸었음.
 print(character)
