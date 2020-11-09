95a329540fdb62288b2ad16671a497b90c58e887
nyoongoon <nyoongoon@naver.com>
2020-11-09 16:22:47 +0900

    exercise_04_4_2_after

--- exercise_04_4_2.py
+++ exercise_04_4_2.py
@@ -1,5 +1,5 @@
-output = [int("{:b}".format(i), 2) for i in range(1, 101) if "{:b}".format(i).count("0") == 1]
-
+output = [i for i in range(1, 101) if "{:b}".format(i).count("0") == 1]
+# i를 2진수로 바꾸었다가 다시 10진수로 바꾸었었는데, 그냥 i사용하면 됌.
 for i in output:
     print("{} : {}".format(i, "{:b}".format(i)))
 
