fff43d8010cba066194b79741fcc15dffbbd3afd
Seokbin Yoon <nyoongoon@naver.com>
2020-11-12 18:13:44 +0900

    exercise_05_2_1_after(못풀음)

--- exercise_05_2_1.py
+++ exercise_05_2_1.py
@@ -1,8 +1,8 @@
 def flatten(data):
-    output = []
+    output = [] # flatten함수를 호출할 때 output 함수 재선언 하는것이 아님!
     for item in data:
         if type(item) == list:
-            output += flatten(data)
+            output += flatten(data) # 리스트간의 대입 연산자를 활용 !!!
         else:
             output.append(item)
     return output
