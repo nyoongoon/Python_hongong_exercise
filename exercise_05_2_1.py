7dd636da00417effb0c0e10d4ae7873bf3f942a1
Seokbin Yoon <nyoongoon@naver.com>
2020-11-12 18:09:36 +0900

    exercise_05_2_1_after(못풀음)

--- exercise_05_2_1.py
+++ exercise_05_2_1.py
@@ -1,20 +1,12 @@
-count = 0
-datas = []
-
 def flatten(data):
-
-    while(data):
-        if type(data[0]) is list:
-            flatten(data[0])
+    output = []
+    for item in data:
+        if type(item) == list:
+            output += flatten(data)
         else:
-            del data[0]
-            datas.append(data[0])
-    return datas
+            output.append(item)
+    return output
 
-
-
-
-
-example = [[1, 2, 3], [4, [5, 6]], 7, [8, 9]]
+example = [[1, 2, 3], [4,[5, 6]], 7, [8, 9]]
 print("원본:", example)
 print("변환:", flatten(example))
